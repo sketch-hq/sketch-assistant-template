36690a6d083f17b00812b2905b15e89c30d978b3
Jed Richards <jedrichards@gmail.com>
2020-07-13 15:03:24 +0100

    feat: support 6.0.0 (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import { AssistantPackage } from '@sketch-hq/sketch-assistant-types'
+import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-import { helloWorldRule } from './rules/hello-world'
+const helloWorld: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report('Hello world')
+  },
+  name: 'sketch-assistant-template/hello-world',
+  title: 'Hello World',
+  description: 'Reports a hello world message',
+}
 
 const assistant: AssistantPackage = [
   async () => {
     return {
       name: 'sketch-assistant-template',
-      rules: [helloWorldRule],
+      rules: [helloWorld],
       config: {
         rules: {
-          [helloWorldRule.name]: { active: true },
+          'sketch-assistant-template/hello-world': { active: true },
         },
       },
     }
